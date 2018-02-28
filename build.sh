d56cb0dc61bf87d9531a83eee288605823d58d17
Krzysztof Skrzynecki <krzysztof@digi.me>
2018-02-28 08:23:04 +0000

    test

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -xe
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # This will: compile the project, run lint, run tests under JVM, package apk, check the code quality and run tests on the device/emulator.
-"$PROJECT_DIR"/gradlew --no-daemon --info clean build -PdisablePreDex -PwithDexcount -Dscan 
+"$PROJECT_DIR"/gradlew --no-daemon --info clean build -PdisablePreDex -PwithDexcount -Dscan
 
 # Disable instrumentation tests for now, Travis is too flaky with Android Emulator.
 # See https://github.com/artem-zinnatullin/qualitymatters/issues/220
